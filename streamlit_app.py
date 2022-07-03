0c56a5167ceb150d6667b80541bf35f40df1e968
moran-shemesh <56132711+moran-shemesh@users.noreply.github.com>
2022-07-03 15:27:07 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,21 @@ elif Property_Area == 'Semiurban' :
    Semiurban = 1
 else :
    Rural=1
+  
+data1={
+    'Gender':Gender,
+    'Married':Married,
+    'Dependents':[class_0,class_1,class_2,class_3],
+    'Education':Education,
+    'ApplicantIncome':ApplicantIncome,
+    'CoapplicantIncome':CoapplicantIncome,
+    'Self Employed':Self_Employed,
+    'LoanAmount':LoanAmount,
+    'Loan_Amount_Term':Loan_Amount_Term,
+    'Credit_History':Credit_History,
+    'Property_Area':[Rural,Urban,Semiurban],
+    }
+
+    feature_list=[ApplicantIncome,CoapplicantIncome,LoanAmount,Loan_Amount_Term,Credit_History,get_value(Gender,gender_dict),get_fvalue(Married),data1['Dependents'][0],data1['Dependents'][1],data1['Dependents'][2],data1['Dependents'][3],get_value(Education,edu),get_fvalue(Self_Employed),data1['Property_Area'][0],data1['Property_Area'][1],data1['Property_Area'][2]]
+
+    single_sample = np.array(feature_list).reshape(1,-1)
