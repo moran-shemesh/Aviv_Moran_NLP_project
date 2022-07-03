b062cf434e64420fdf2712e90e380e0c121bccf4
moran-shemesh <56132711+moran-shemesh@users.noreply.github.com>
2022-07-03 11:57:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import time
 # kaggle_dir = f"/content/drive/MyDrive/HIT/NLP/Final_Project/cnn_daily_mail_dataset/kaggle/cnn_dailymail"
 # dummy_train_df = pd.read_csv('data/validation.csv', nrows=10)
 
-st.title ("Summarization - CNN / Daily Mail")
-st.header("Aviv Lazar & Moran Shemesh")
+st.sidebar.title ("Summarization - CNN / Daily Mail")
+st.sidebar.header("Aviv Lazar & Moran Shemesh")
 # st.markdown("this is the header")
 # st.subheader("this is the subheader")
 # st.caption("this is the caption")
@@ -15,18 +15,19 @@ st.header("Aviv Lazar & Moran Shemesh")
 
 # input widgets:
 # st.checkbox('yes')
-st.button('Start')
-st.radio('Pickc a model',['RankText', 'Bart'])
+st.sidebar.button('Start')
+st.sidebar.radio('Pickc a model',['RankText', 'Bart'])
 # st.selectbox('Pick your gender',['Male','Female'])
 # st.multiselect('choose a model',['RankText', 'Bart'])
-st.select_slider("What's your grade?", ['Bad', 'Good', 'Excellent'])
+st.sidebar.select_slider("What's your grade?", ['Bad', 'Good', 'Excellent'])
 # st.slider('Pick a number', 0,50)
-# progress bar:
-st.progress(10)
-with st.spinner('Working on it...'):
-    time.sleep(10)
 
-st.success("You did it !")
+# progress bar:
+# st.progress(10)
+# with st.spinner('Working on it...'):
+#     time.sleep(10)
+
+st.sidebar.success("You did it !")
 # st.write(dummy_train_df.head(10))
 
 
