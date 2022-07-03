ccc5237a55036e5c91aacf4f78ef0e5a7b6aeb1a
moran-shemesh <56132711+moran-shemesh@users.noreply.github.com>
2022-07-03 15:13:30 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,13 +10,6 @@ import numpy as np
 
 # kaggle_dir = f"/content/drive/MyDrive/HIT/NLP/Final_Project/cnn_daily_mail_dataset/kaggle/cnn_dailymail"
 # dummy_train_df = pd.read_csv('data/validation.csv', nrows=10)
-# [theme]
-primaryColor="#F63366"
-backgroundColor="#FFFFFF"
-secondaryBackgroundColor="#F0F2F6"
-textColor="#262730"
-font="sans serif"
-
 
 st.markdown(
      """
@@ -69,3 +62,41 @@ st.line_chart(df)
 # fig, ax = plt.subplots()
 # ax.hist(rand, bins=15)
 # st.pyplot(fig)
+
+
+st.subheader('Sir/Mme , YOU need to fill all necessary informations in order    to get a reply to your loan request !')
+    st.sidebar.header("Informations about the client :")
+    gender_dict = {"Male":1,"Female":2}
+    feature_dict = {"No":1,"Yes":2}
+    edu={'Graduate':1,'Not Graduate':2}
+    prop={'Rural':1,'Urban':2,'Semiurban':3}
+    ApplicantIncome=st.sidebar.slider('ApplicantIncome',0,10000,0,)
+    CoapplicantIncome=st.sidebar.slider('CoapplicantIncome',0,10000,0,)
+    LoanAmount=st.sidebar.slider('LoanAmount in K$',9.0,700.0,200.0)
+    Loan_Amount_Term=st.sidebar.selectbox('Loan_Amount_Term',(12.0,36.0,60.0,84.0,120.0,180.0,240.0,300.0,360.0))
+    Credit_History=st.sidebar.radio('Credit_History',(0.0,1.0))
+    Gender=st.sidebar.radio('Gender',tuple(gender_dict.keys()))
+    Married=st.sidebar.radio('Married',tuple(feature_dict.keys()))
+    Self_Employed=st.sidebar.radio('Self Employed',tuple(feature_dict.keys()))
+    Dependents=st.sidebar.radio('Dependents',options=['0','1' , '2' , '3+'])
+    Education=st.sidebar.radio('Education',tuple(edu.keys()))
+    Property_Area=st.sidebar.radio('Property_Area',tuple(prop.keys()))
+
+
+    class_0 , class_3 , class_1,class_2 = 0,0,0,0
+    if Dependents == '0':
+        class_0 = 1
+    elif Dependents == '1':
+        class_1 = 1
+    elif Dependents == '2' :
+        class_2 = 1
+    else:
+        class_3= 1
+
+    Rural,Urban,Semiurban=0,0,0
+    if Property_Area == 'Urban' :
+        Urban = 1
+    elif Property_Area == 'Semiurban' :
+        Semiurban = 1
+    else :
+        Rural=1
