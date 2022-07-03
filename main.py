9bad9639b99a36aa4a13f678b677938477ed2e02
moran-shemesh <56132711+moran-shemesh@users.noreply.github.com>
2022-07-03 11:39:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 
 # kaggle_dir = f"/content/drive/MyDrive/HIT/NLP/Final_Project/cnn_daily_mail_dataset/kaggle/cnn_dailymail"
-dummy_train_df = pd.read_csv(data/validation.csv', nrows=10)
+dummy_train_df = pd.read_csv('data/validation.csv', nrows=10)
 
 
 st.write(dummy_train_df.head(10))
