91c0a2d90da6c9c9a46421b19ed368ee3f5430fb
moran-shemesh <56132711+moran-shemesh@users.noreply.github.com>
2022-07-03 11:50:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ st.header("Aviv Lazar & Moran Shemesh")
 # input widgets:
 # st.checkbox('yes')
 st.button('Start')
-st.radio('Pickc a model',[RankText', 'Bart'])
+st.radio('Pickc a model',['RankText', 'Bart'])
 # st.selectbox('Pick your gender',['Male','Female'])
 # st.multiselect('choose a model',['RankText', 'Bart'])
 st.select_slider("What's your grade?", ['Bad', 'Good', 'Excellent'])
