f27a96f020ec114a49c4381140c3b193e9c0f7f7
moran-shemesh <56132711+moran-shemesh@users.noreply.github.com>
2022-07-03 11:45:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,17 @@ import streamlit as st
 
 
 # kaggle_dir = f"/content/drive/MyDrive/HIT/NLP/Final_Project/cnn_daily_mail_dataset/kaggle/cnn_dailymail"
-dummy_train_df = pd.read_csv('data/validation.csv', nrows=10)
+# dummy_train_df = pd.read_csv('data/validation.csv', nrows=10)
+
+st.title ("Summarization - CNN / Daily Mail")
+st.header("Aviv Lazar & Moran Shemesh")
+# st.markdown("this is the header")
+# st.subheader("this is the subheader")
+# st.caption("this is the caption")
+# st.code("x=2021")
+# st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 
 
-st.write(dummy_train_df.head(10))
+# st.write(dummy_train_df.head(10))
 
 
