10cca5aa5479fae69ccc32ce5b7f2d73f4c39741
moran-shemesh <56132711+moran-shemesh@users.noreply.github.com>
2022-07-06 15:39:20 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,8 @@ import torch
 #kaggle_dir = f"/content/drive/MyDrive/HIT/NLP/Final_Project/cnn_daily_mail_dataset/kaggle/cnn_dailymail"
 # dummy_train_df = pd.read_csv('data/validation.csv', nrows=10)
 
-url = ' 16XYO5xFM16hXMBshK4orssiTDkhggo98'
-path = 'https://drive.google.com/uc?export=download&id='+url.split('/')[-2]
+
+path = 'https://drive.google.com/uc?export=download&16XYO5xFM16hXMBshK4orssiTDkhggo98'
 
 # /view?usp=sharing
 
