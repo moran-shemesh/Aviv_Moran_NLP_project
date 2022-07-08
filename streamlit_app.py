d967cf2eacd01e5f9ff89480f35813624ab44170
moran-shemesh <56132711+moran-shemesh@users.noreply.github.com>
2022-07-08 17:38:23 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ import streamlit as st
 import time
 import numpy as np
 from transformers import pipeline
+from fastai.text.all import *
 from blurr.text.data.all import *
 from blurr.text.modeling.all import *
 
