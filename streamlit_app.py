e7902fd32bf897ec9315210dafe3d2fd56b402b3
moran-shemesh <56132711+moran-shemesh@users.noreply.github.com>
2022-07-13 20:46:11 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import streamlit as st
 # import time
 import numpy as np
 from transformers import BartForConditionalGeneration
-
+from huggingface_hub import from_pretrained_fastai
 # model = BartForConditionalGeneration.from_pretrained("Moran/Moran_Aviv_Bart",from_tf=True)
 
 # inf_learn = load_learner(fname='Moran/Moran_Aviv_Bart')
