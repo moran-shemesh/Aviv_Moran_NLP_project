f83410733d9412b84aa19c93b4abdecbb54f56b5
moran-shemesh <56132711+moran-shemesh@users.noreply.github.com>
2022-07-15 16:37:42 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,13 +20,15 @@ def textrank(corpus, ratio=0.2):
 
 # inf_learn = from_pretrained_fastai(repo_id)
 
+summary = ""
+st.caption(" © Aviv Lazar & Moran Shemesh")
 
 with st.spinner('Please wait...'):
     time.sleep(10)
 
-st.title ("Text Summarization") 
-model_type = st.radio('Pick a model',['RankText', 'Bart'])
-text = st.text_area('Enter or paste your text')
+container_main.title ("Text Summarization") 
+model_type = container_main.radio('Pick a model',['RankText', 'Bart'])
+user_text = container_main.text_area('Enter or paste text to summarize')
 
 def start_summarize(long_text, model):
   num_of_sentences = len(sent_tokenize(long_text))
@@ -38,40 +40,18 @@ def start_summarize(long_text, model):
     else:
       if model=="Bart":
         summary = "Sorry, we don't support Bart here. Please try summarize with Bart at the following link: https://huggingface.co/spaces/Moran/Aviv_Moran_Summarization . For more details aboute the model visit the model's card https://huggingface.co/Aviv/Moran_Aviv_Bart"
-    # repo_id = "Aviv/Moran_Aviv_Bart"
-    # inf_learn = from_pretrained_fastai(repo_id)
+#       streamlit limits are pass while running it, so we will write the code as comments:
+#         repo_id = "Aviv/Moran_Aviv_Bart"
+#         inf_learn = from_pretrained_fastai(repo_id)  
+#         summary = inf_learn.blurr_generate([long_text])[0]['generated_texts']  
   else:
     summary = long_text
-  st.success(summary)
-#   time.sleep(10)
-  grade = st.select_slider('What do you think about the summary?', options=['Bad', 'Good', 'Excellent'], value=('Good'))
+  container_summarization.markdown("Summary")
+  container_summarization.success(summary)
+  time.sleep(9)
+  grade = container_summarization.select_slider('What do you think about the summary?', options=['Bad', 'Good', 'Excellent'], value=('Good'))
+
 
   
-st.button('Summarize', on_click=start_summarize, args=(text, model_type, ) )
-# increment = st.button('Increment', on_click=increment_counter,
-#     args=(increment_value, ))
-
-
-# try:
-#   print(x)
-# except:
-#   print("An exception occurred")
-
-#   try run the model one of the next code:
-# inf_learn.blurr_generate([text])
-# summary = inf_learn.blurr_summarize(text)
-
-# st.title(text)
-
-# st.title(inf_learn.blurr_summarize(text))
-
-
-
-
-st.caption(" © Aviv Lazar & Moran Shemesh")
-# url = 'https://drive.google.com/file/d/16XYO5xFM16hXMBshK4orssiTDkhggo98/view?usp=sharing'
-# filename = url.split('/')[-1]
-
-
-# We will not use bart here because loading the model crosses streamlits limit. We get the same error as described here: https://discuss.streamlit.io/t/error-deploying-app-error-checking-streamlit-healthz-connection-refused/9063
-
+container_main.button('Summarize', on_click=start_summarize, args=(user_text, model_type, ) )
+######################
